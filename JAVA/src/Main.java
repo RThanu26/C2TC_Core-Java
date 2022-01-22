d426fefcb30586cae1e59b55f40dd50bfa9cb76e
Thanusha <Thanusha@LAPTOP-A943BI8I>
2022-01-22 18:47:24 +0530

    first commit

---
+++ JAVA/src/Main.java
@@ -0,0 +1,30 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Cricketer c = new Cricketer();
+		System.out.println(c.name);
+		// TODO Auto-generated method stub
+
+	}
+
+}
+class Cricketer //secondary class
+{
+    String name = "Dhoni";
+
+	int age = 39;
+
+	String color = "Brown";
+
+	String role = "Batsman";
+
+	String nationality = "Indian";
+
+	void Display(){
+
+	System.out.println("I LOVE Criket");
+
+	}//end of method
+
+	}//end of class
